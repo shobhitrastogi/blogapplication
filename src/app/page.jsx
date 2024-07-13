651042171e097243f25b9bb2454660cede4202fa
Shobhit Rastogi <rshobhit255@gmail.com>
2024-07-13 15:44:33 +0530

    change the image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,11 +5,10 @@ const Home = () => {
   return (
     <div className={styles.container}>
       <div className={styles.textContainer}>
-        <h1 className={styles.title}>Shobhit Rastogi</h1>
+        <h1 className={styles.title}>Creative Thoughts Agency.</h1>
         <p className={styles.desc}>
-        As a passionate and innovative MERN Stack Developer, I thrive on crafting seamless and dynamic web applications. With expertise in MongoDB, Express.js, Node.js, React, TypeScript, Tailwind CSS, authentication, and Next.js, I transform ideas into reality with precision and creativity.
-
-
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero
+          blanditiis adipisci minima reiciendis a autem assumenda dolore.
         </p>
         <div className={styles.buttons}>
           <button className={styles.button}>Learn More</button>
@@ -20,7 +19,7 @@ const Home = () => {
         </div>
       </div>
       <div className={styles.imgContainer}>
-        <Image src="/heri.jpg" alt="" fill className={styles.heroImg}/>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg}/>
       </div>
     </div>
   );
